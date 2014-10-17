c5d66f039847b84a8f40949f8414df9450b95303
lloda <lloda@sarc.name>
2014-10-17 16:16:43 +0200

    Wrap gemv functions
    * mod/ffi/cblas.scm (scalar->cblas-arg, stride, dim, check-arrays-AXY): new
      functions.
    
      (define-axpy): merged from define-axpy-real, define-axpy-complex.
    
      (define-gemv): to define wrappers.
    
      (sgemv!, dgemv!, cgemv!, zgemv!): wrappers for cblas_sgemv, cblas_dgemv,
      cblas_cgemv, cblas_zgemv.
    
    * test/test-ffi-cblas.scm (make-M-c-order, make-M-fortran-order,
      make-M-strided-type, make-M-offset, fill-A2!): new functions to create example
      matrices.
    
      (test-axpy, test-dot): compute tag from make-... args. Fix users.
    
      (array-copy, ref-gemv!, test-gemv): new functions for gemv tests. Use these
      to test sgemv!, dgemv!, cgemv!, zgemv!.

--- mod/ffi/cblas.scm
+++ mod/ffi/cblas.scm
@@ -8,7 +8,7 @@
 ; later version.
 
 (define-module (ffi cblas))
-(import (system foreign) (srfi srfi-1))
+(import (system foreign) (srfi srfi-1) (srfi srfi-11))
 
 ; @TODO As an alternative go through installation.
 (define libcblas (dynamic-link (let ((lpath (getenv "GUILE_FFI_CBLAS_LIBCBLAS_PATH")))
@@ -39,11 +39,24 @@
   (case srfi4-type
     ((f32) float)
     ((f64) double)
+    ((c32 c64) '*)
     (else (throw 'no-ffi-type-for-type srfi4-type))))
 
 (define (pointer-to-first A)
   (bytevector->pointer (shared-array-root A)
                        (* (shared-array-offset A) (srfi4-type-size (array-type A)))))
+
+(define (scalar->cblas-arg srfi4-type a)
+  (case srfi4-type
+    ((f32 f64) a)
+    ((c32 c64) (pointer-to-first (make-typed-array srfi4-type a)))
+    (else (throw 'bad-array-type srfi4-type))))
+
+(define (stride A i)
+  (list-ref (shared-array-increments A) i))
+
+(define (dim A i)
+  (list-ref (array-dimensions A) i))
 
 ;; Consider http://wiki.call-cc.org/eggref/4/blas#usage
 ;; The three levels would be:
@@ -59,6 +72,10 @@
 ;; 3) (name ...) ~ (xxx ...) converts arguments as far as possible. Returns new
 ;; typed arrays.
 
+; -----------------------------
+; sum_i(a_i * b_i): sdot ddot cdotu cdotc zdotu zdotc
+; -----------------------------
+
 (define-syntax define-dot-real
   (syntax-rules ()
     ((_ name srfi4-type cblas-name cblas-name-string)
@@ -69,8 +86,8 @@
        (define (name A B)
          (check-2-arrays A B 1 srfi4-type)
          (cblas-name (array-length A)
-                     (pointer-to-first A) (first (shared-array-increments A))
-                     (pointer-to-first B) (first (shared-array-increments B))))))))
+                     (pointer-to-first A) (stride A 0)
+                     (pointer-to-first B) (stride B 0)))))))
 
 ; double cblas_ddot (const int N, const double *X, const int incX, const double *Y, const int incY)
 (define-dot-real ddot 'f64 cblas_ddot "cblas_ddot")
@@ -91,8 +108,8 @@
          (check-2-arrays A B 1 srfi4-type)
          (let ((C (make-typed-array srfi4-type *unspecified*)))
            (cblas-name (array-length A)
-                       (pointer-to-first A) (first (shared-array-increments A))
-                       (pointer-to-first B) (first (shared-array-increments B))
+                       (pointer-to-first A) (stride A 0)
+                       (pointer-to-first B) (stride B 0)
                        (pointer-to-first C))
            (array-ref C)))))))
 
@@ -112,8 +129,12 @@
 (define-dot-complex cdotc 'c32 cblas_cdotc_sub "cblas_cdotc_sub")
 (export cdotc cblas_cdotc_sub)
 
+; -----------------------------
+; a*x + y -> y: saxpy daxpy caxpy zaxpy
+; -----------------------------
+
 ; @TODO pointer-to-this-value support in the ffi, for old C decls that take double * for complex.
-(define-syntax define-axpy-real
+(define-syntax define-axpy
   (syntax-rules ()
     ((_ name srfi4-type cblas-name cblas-name-string)
      (begin
@@ -122,36 +143,106 @@
                                               (list int (srfi4-type->type srfi4-type) '* int '* int)))
        (define (name a X Y)
          (check-2-arrays X Y 1 srfi4-type)
-         (cblas-name (array-length X) a
-                     (pointer-to-first X) (first (shared-array-increments X))
-                     (pointer-to-first Y) (first (shared-array-increments Y))))))))
+         (cblas-name (array-length X) (scalar->cblas-arg srfi4-type a)
+                     (pointer-to-first X) (stride X 0)
+                     (pointer-to-first Y) (stride Y 0)))))))
 
 ; void cblas_daxpy (const int N, const double alpha, const double *X, const int incX, double *Y, const int incY)
-(define-axpy-real daxpy! 'f64 cblas_daxpy "cblas_daxpy")
+(define-axpy daxpy! 'f64 cblas_daxpy "cblas_daxpy")
 (export cblas_daxpy daxpy!)
 
 ; void cblas_saxpy (const int N, const float alpha, const float *X, const int incX, float *Y, const int incY)
-(define-axpy-real saxpy! 'f32 cblas_saxpy "cblas_saxpy")
+(define-axpy saxpy! 'f32 cblas_saxpy "cblas_saxpy")
 (export cblas_saxpy saxpy!)
 
-(define-syntax define-axpy-complex
-  (syntax-rules ()
-    ((_ name srfi4-type cblas-name cblas-name-string)
-     (begin
-       (define cblas-name (pointer->procedure void
-                                              (dynamic-func cblas-name-string libcblas)
-                                              (list int '* '* int '* int)))
-       (define (name a X Y)
-         (check-2-arrays X Y 1 srfi4-type)
-         (cblas-name (array-length X)
-                     (pointer-to-first (make-typed-array srfi4-type a))
-                     (pointer-to-first X) (first (shared-array-increments X))
-                     (pointer-to-first Y) (first (shared-array-increments Y))))))))
-
 ; void cblas_zaxpy (const int N, const void *alpha, const void *X, const int incX, void *Y, const int incY)
-(define-axpy-complex zaxpy! 'c64 cblas_zaxpy "cblas_zaxpy")
+(define-axpy zaxpy! 'c64 cblas_zaxpy "cblas_zaxpy")
 (export cblas_zaxpy zaxpy!)
 
 ; void cblas_caxpy (const int N, const void *alpha, const void *X, const int incX, void *Y, const int incY)
-(define-axpy-complex caxpy! 'c32 cblas_caxpy "cblas_caxpy")
+(define-axpy caxpy! 'c32 cblas_caxpy "cblas_caxpy")
 (export cblas_caxpy caxpy!)
+
+; -----------------------------
+; alpha*sum_j(A_{ij}*X_j) + beta*Y_i -> Y_i: sgemv dgemv cgemv zgemv
+; -----------------------------
+
+; CBLAS_ORDER
+(define CblasRowMajor 101)
+(define CblasColMajor 102)
+
+; CBLAS_TRANSPOSE
+(define CblasNoTrans 111)
+(define CblasTrans 112)
+(define CblasConjTrans 113)
+
+; CBLAS_UPLO
+(define CblasUpper 121)
+(define CblasLower 122)
+
+; CBLAS_DIAG
+(define CblasNonUnit 131)
+(define CblasUnit 132)
+
+; CBLAS_SIDE
+(define CblasLeft 141)
+(define CblasRight 142)
+
+(define (lead-and-order A)
+  (let ((A-strides (shared-array-increments A)))
+    (cond ((= 1 (first A-strides))
+           (values (second A-strides) CblasColMajor))
+          ((= 1 (second A-strides))
+           (values (first A-strides) CblasRowMajor))
+          (else (throw 'unsupported-stride-for-cblas-matrix)))))
+
+(define (check-arrays-AXY A X Y type)
+  (check-array A 2 type)
+  (check-array X 1 type)
+  (check-array Y 1 type)
+  (unless (= (dim A 1) (array-length X))
+    (throw 'bad-size-X (dim A 1) (array-length X)))
+  (unless (= (dim A 0) (array-length Y))
+    (throw 'bad-size-X (dim A 0) (array-length Y))))
+
+(define-syntax define-gemv
+  (syntax-rules ()
+    ((_ name srfi4-type cblas-name cblas-name-string)
+     (begin
+       (define cblas-name (pointer->procedure
+                           void
+                           (dynamic-func cblas-name-string libcblas)
+                           (list int int int int (srfi4-type->type srfi4-type) '* int
+                                 '* int (srfi4-type->type srfi4-type) '* int)))
+       (define (name alpha A X beta Y)
+         (check-arrays-AXY A X Y srfi4-type)
+         (let-values (((A-lead A-order) (lead-and-order A)))
+           (cblas-name A-order CblasNoTrans
+                       (dim A 0) (dim A 1) (scalar->cblas-arg srfi4-type alpha)
+                       (pointer-to-first A) A-lead
+                       (pointer-to-first X) (stride X 0) (scalar->cblas-arg srfi4-type beta)
+                       (pointer-to-first Y) (stride Y 0))))))))
+
+; void cblas_dgemv (const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+;                   const int M, const int N, const double alpha, const double *A, const int lda,
+;                   const double *X, const int incX, const double beta, double *Y, const int incY)
+(define-gemv dgemv! 'f64 cblas_dgemv "cblas_dgemv")
+(export cblas_dgemv dgemv!)
+
+; void cblas_sgemv (const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+;                   const int M, const int N, const float alpha, const float *A, const int lda,
+;                   const float *X, const int incX, const float beta, float *Y, const int incY)
+(define-gemv sgemv! 'f32 cblas_sgemv "cblas_sgemv")
+(export cblas_sgemv sgemv!)
+
+; void cblas_zgemv (const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+;                   const int M, const int N, const void * alpha, const void *A, const int lda,
+;                   const void *X, const int incX, const void * beta, void *Y, const int incY)
+(define-gemv zgemv! 'c64 cblas_zgemv "cblas_zgemv")
+(export cblas_zgemv zgemv!)
+
+; void cblas_cgemv (const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+;                   const int M, const int N, const void * alpha, const void *A, const int lda,
+;                   const void *X, const int incX, const void * beta, void *Y, const int incY)
+(define-gemv cgemv! 'c32 cblas_cgemv "cblas_cgemv")
+(export cblas_cgemv cgemv!)
--- test/test-ffi-cblas.scm
+++ test/test-ffi-cblas.scm
@@ -9,15 +9,17 @@
 
 (import (ffi cblas) (srfi srfi-64) (srfi srfi-1) (ice-9 match))
 
+; ---------------------------------
 ; Various sorts of arrays.
+; ---------------------------------
 
-(define (make-A-compact type)
+(define (make-v-compact type)
   (make-typed-array type *unspecified* 10))
 
-(define (make-A-strided type)
+(define (make-v-strided type)
   (make-shared-array (make-typed-array type *unspecified* 20) (lambda (i) (list (* i 2))) 10))
 
-(define (make-A-offset type)
+(define (make-v-offset type)
   (make-shared-array (make-typed-array type *unspecified* 12) (lambda (i) (list (+ i 2))) 10))
 
 (define (fill-A1! A)
@@ -34,39 +36,30 @@
     (else (throw 'bad-array-type (array-type A))))
   A)
 
-; Basic tests.
+(define (make-M-c-order type)
+  (make-typed-array type *unspecified* 10 10))
 
-(define (conj a) (make-rectangular (real-part a) (- (imag-part a))))
-(define dotc-step (lambda (a b c) (+ c (* (conj a) b))))
-(define dotu-step (lambda (a b c) (+ c (* a b))))
+(define (make-M-fortran-order type)
+  (transpose-array (make-typed-array type *unspecified* 10 10) 1 0))
 
-(define (test-dot srfi4-type dot dot-step tag make-A make-B)
-  (let ((case-name (format #f "~a, ~a" (procedure-name dot) tag))
-        (A (fill-A1! (make-A srfi4-type)))
-        (B (fill-B1! (make-B srfi4-type))))
-    (test-begin case-name)
-    (test-equal (dot A B) (fold dot-step 0. (array->list A) (array->list B)))
-    (test-end case-name)))
+(define (make-M-strided type)
+  (make-shared-array (make-typed-array type *unspecified* 20 10) (lambda (i j) (list (* i 2) j)) 10 10))
 
-(map
- (match-lambda
-     ((srfi4-type dot dot-step)
-      (test-dot srfi4-type dot dot-step "compact-compact" make-A-compact make-A-compact)
-      (test-dot srfi4-type dot dot-step "compact-compact" make-A-compact make-A-compact)
-      (test-dot srfi4-type dot dot-step "compact-strided" make-A-compact make-A-strided)
-      (test-dot srfi4-type dot dot-step "compact-offset" make-A-compact make-A-offset)
-      (test-dot srfi4-type dot dot-step "strided-compact" make-A-strided make-A-compact)
-      (test-dot srfi4-type dot dot-step "strided-strided" make-A-strided make-A-strided)
-      (test-dot srfi4-type dot dot-step "strided-offset" make-A-strided make-A-offset)
-      (test-dot srfi4-type dot dot-step "offset-compact" make-A-offset make-A-compact)
-      (test-dot srfi4-type dot dot-step "offset-strided" make-A-offset make-A-strided)
-      (test-dot srfi4-type dot dot-step "offset-offset" make-A-offset make-A-offset)))
- `((f64 ,ddot ,dotu-step)
-   (f32 ,sdot ,dotu-step)
-   (c64 ,zdotc ,dotc-step)
-   (c64 ,zdotu ,dotu-step)
-   (c32 ,cdotu ,dotu-step)
-   (c32 ,cdotu ,dotu-step)))
+(define (make-M-offset type)
+  (make-shared-array (make-typed-array type *unspecified* 12 13) (lambda (i j) (list (+ i 2) (+ j 3))) 10 10))
+
+(define (fill-A2! A)
+  (case (array-type A)
+    ((f32 f64) (array-index-map!
+                A (lambda (i j) (+ 4 (* i 1) (* j j 2)))))
+    ((c32 c64) (array-index-map!
+                A (lambda (i j) (make-rectangular (+ 4 (* i 1) (* j j 2)) (+ -3 (* i i 1) (* j 2) -4)))))
+    (else (throw 'bad-array-type (array-type A))))
+  A)
+
+; ---------------------------------
+; Test types
+; ---------------------------------
 
 (define* (test-approximate-array test expected err)
   (test-begin "approximate array")
@@ -74,10 +67,49 @@
                   test expected)
   (test-end "approximate array"))
 
-(define (test-axpy srfi4-type axpy! tag make-A make-B)
-  (let ((case-name (format #f "~a, ~a" (procedure-name axpy!) tag))
-        (A (fill-A1! (make-A srfi4-type)))
-        (B (fill-B1! (make-B srfi4-type))))
+; ---------------------------------
+; sdot ddot cdotc cdotu zdotc zdotu
+; ---------------------------------
+
+(define (conj a) (make-rectangular (real-part a) (- (imag-part a))))
+(define dotc-step (lambda (a b c) (+ c (* (conj a) b))))
+(define dotu-step (lambda (a b c) (+ c (* a b))))
+
+(define (test-dot srfi4-type dot dot-step make-A make-B)
+  (let* ((tag (format #f "~a:~a" (procedure-name make-A) (procedure-name make-B)))
+         (case-name (format #f "~a, ~a" (procedure-name dot) tag))
+         (A (fill-A1! (make-A srfi4-type)))
+         (B (fill-B1! (make-B srfi4-type))))
+    (test-begin case-name)
+    (test-equal (dot A B) (fold dot-step 0. (array->list A) (array->list B)))
+    (test-end case-name)))
+
+(map
+ (match-lambda
+     ((srfi4-type dot dot-step)
+      (for-each
+       (lambda (make-X)
+         (for-each
+          (lambda (make-Y)
+            (test-dot srfi4-type dot dot-step make-X make-Y))
+          (list make-v-compact make-v-offset make-v-strided)))
+       (list make-v-compact make-v-offset make-v-strided))))
+ `((f64 ,ddot ,dotu-step)
+   (f32 ,sdot ,dotu-step)
+   (c64 ,zdotc ,dotc-step)
+   (c64 ,zdotu ,dotu-step)
+   (c32 ,cdotu ,dotu-step)
+   (c32 ,cdotu ,dotu-step)))
+
+; ---------------------------------
+; saxpy daxpy caxpy zaxpy
+; ---------------------------------
+
+(define (test-axpy srfi4-type axpy! make-A make-B)
+  (let* ((tag (format #f "~a:~a" (procedure-name make-A) (procedure-name make-B)))
+         (case-name (format #f "~a, ~a" (procedure-name axpy!) tag))
+         (A (fill-A1! (make-A srfi4-type)))
+         (B (fill-B1! (make-B srfi4-type))))
     (let ((Alist (array->list A))
           (Blist (array->list B)))
       (test-begin case-name)
@@ -93,20 +125,71 @@
 (map
  (match-lambda
      ((srfi4-type axpy!)
-      (test-axpy srfi4-type axpy! "compact-compact" make-A-compact make-A-compact)
-      (test-axpy srfi4-type axpy! "compact-compact" make-A-compact make-A-compact)
-      (test-axpy srfi4-type axpy! "compact-strided" make-A-compact make-A-strided)
-      (test-axpy srfi4-type axpy! "compact-offset" make-A-compact make-A-offset)
-      (test-axpy srfi4-type axpy! "strided-compact" make-A-strided make-A-compact)
-      (test-axpy srfi4-type axpy! "strided-strided" make-A-strided make-A-strided)
-      (test-axpy srfi4-type axpy! "strided-offset" make-A-strided make-A-offset)
-      (test-axpy srfi4-type axpy! "offset-compact" make-A-offset make-A-compact)
-      (test-axpy srfi4-type axpy! "offset-strided" make-A-offset make-A-strided)
-      (test-axpy srfi4-type axpy! "offset-offset" make-A-offset make-A-offset)))
+      (for-each
+       (lambda (make-X)
+         (for-each
+          (lambda (make-Y)
+            (test-axpy srfi4-type axpy! make-X make-Y))
+          (list make-v-compact make-v-offset make-v-strided)))
+       (list make-v-compact make-v-offset make-v-strided))))
  `((f64 ,daxpy!)
    (f32 ,saxpy!)
    (c64 ,zaxpy!)
    (c32 ,caxpy!)))
+
+; ---------------------------------
+; sgemv dgemv cgemv zgemv
+; ---------------------------------
+
+(define (array-copy A)
+  (let ((B (apply make-typed-array (array-type A) *unspecified* (array-dimensions A))))
+    (array-copy! A B)
+    B))
+
+(define (ref-gemv! alpha A X beta Y)
+  (match (array-dimensions A)
+    ((M N)
+     (do ((i 0 (+ i 1))) ((= i M))
+       (array-set! Y (* beta (array-ref Y i)) i)
+       (do ((j 0 (+ j 1))) ((= j N))
+         (array-set! Y (+ (array-ref Y i) (* alpha (array-ref A i j) (array-ref X j))) i)))
+     Y)))
+
+(define (test-gemv srfi4-type gemv! make-A make-X make-Y)
+  (let* ((tag (format #f "~a:~a:~a" (procedure-name make-A) (procedure-name make-X) (procedure-name make-Y)))
+         (case-name (format #f "~a, ~a" (procedure-name gemv!) tag))
+         (A (fill-A2! (make-A srfi4-type)))
+         (X (fill-A1! (make-X srfi4-type)))
+         (Y (fill-B1! (make-Y srfi4-type))))
+    (let ((A1 (array-copy A))
+          (X1 (array-copy X)))
+      (test-begin case-name)
+      (let ((Y1 (array-copy Y))
+            (Y2 (array-copy Y)))
+        (gemv! 2. A X 3. Y1)
+        (ref-gemv! 2. A X 3. Y2)
+        (test-equal Y1 Y2)
+        (test-equal A A1)
+        (test-equal X X1)
+        (test-end case-name)))))
+
+(map
+ (match-lambda
+     ((srfi4-type gemv!)
+      (for-each
+       (lambda (make-A)
+         (for-each
+          (lambda (make-X)
+            (for-each
+             (lambda (make-Y)
+               (test-gemv srfi4-type gemv! make-A make-X make-Y))
+             (list make-v-compact make-v-offset make-v-strided)))
+          (list make-v-compact make-v-offset make-v-strided)))
+       (list make-M-c-order make-M-fortran-order make-M-offset make-M-strided))))
+ `((f64 ,dgemv!)
+   (f32 ,sgemv!)
+   (c64 ,zgemv!)
+   (c32 ,cgemv!)))
 
 (unless (zero? (test-runner-fail-count (test-runner-current)))
   (error "FAILED test-ffi-cblas.csm"))
